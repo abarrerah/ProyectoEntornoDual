39343b3e20eea734e6c03fa67600ca3bd3330114
Abraham Barrera Herrera <abarrerh@everis.com>
2020-04-24 13:15:38 +0200

    Esto es el segundo commit. Versión del programa 1.1

--- src/Test/PenetrationTest.java
+++ src/Test/PenetrationTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Model.AntiAir;
+
 class PenetrationTest {
 
 	@Test
 	void testDamagePenetration() {
-		fail("Not yet implemented");
+		AntiAir aa=new AntiAir("40mm Gesher", "1121A", "bajo", 2,"SS George", 4, 5, 600);
+		aa.damagePenetration();
+		assertEquals(72,aa.damagePenetration());
+		
+		
 	}
 
 }
